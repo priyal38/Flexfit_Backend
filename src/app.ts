3b9b96c5695a39c092d72ce3680895c5c5b6d27d
Priyal Thummar <thummapr@SYNOVERGETECH.COM>
2024-04-05 17:19:08 +0530

    domain added

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 app.use(json());
 app.use(cors(
     {
-        origin:["http://localhost:3000" , "http://localhost"],
+        origin:[ "https://flexfit-fitness.vercel.app/" , "http://localhost:3000" , "http://localhost"],
         credentials:true
     }
 )); 
